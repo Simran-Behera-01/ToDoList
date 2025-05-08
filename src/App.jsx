1e87df9b9ab300d7ef2ca356a953aac49dd5be04
Simran-Behera-01 <208r1a67b2@gmail.com>
2025-05-09 00:33:38 +0530

    Added To Do actions for deleting and completing todos.

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,57 @@ import ToDoList from "./components/ToDoList";
 
 function App() {
   const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState([])
+  const [todoList, setTodoList] = useState([]);
+  const [completedTodoList, setCompletedTodoList] = useState([]);
 
   function todoInput(e) {
     setTodo(e.target.value);
   }
 
-  function addToDo()
-  {
-    setTodoList((prevToDo) => [...prevToDo, todo]);
-    setTodo('');
+  function addToDo() {
+    if (todo != "") {
+      setTodoList((prevToDos) => [
+        ...prevToDos,
+        { todotext: todo, completed: false },
+      ]);
+      setTodo("");
+    }
+  }
+
+  function deleteToDo(todoIndex) {
+    setTodoList((prevToDos) =>
+      prevToDos.filter(
+        (prevToDos, prevToDosIndex) => prevToDosIndex != todoIndex
+      )
+    );
+  }
+
+  function handleCompletedToDo(completedTodo, todoIndex) {
+    completedTodo.completed = true;
+    setCompletedTodoList((completedTodos) => [
+      ...completedTodos,
+      completedTodo,
+    ]);
+    deleteToDo(todoIndex);
   }
 
   return (
     <>
       <div>
         <h1>To Do List</h1>
-      <ToDoContext.Provider value={{todo,todoList,todoInput,addToDo}}>
-        <ToDoInput/>
-        <ToDoList/>
-      </ToDoContext.Provider>
+        <ToDoContext.Provider
+          value={{
+            todo,
+            todoList,
+            todoInput,
+            addToDo,
+            deleteToDo,
+            handleCompletedToDo,
+          }}
+        >
+          <ToDoInput />
+          <ToDoList />
+        </ToDoContext.Provider>
       </div>
     </>
   );
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,13 +1,25 @@
-import React,{useContext} from 'react'
-import { ToDoContext } from '../context/ToDoContext'
+import React, { useContext } from "react";
+import { ToDoContext } from "../context/ToDoContext";
 
 function ToDoList() {
-    const {todoList} = useContext(ToDoContext);
+  const { todoList, deleteToDo, handleCompletedToDo } = useContext(ToDoContext);
   return (
     <ul>
-    {todoList.map((todo,index) => <li key={index}>{todo}</li>)}
+      {todoList.map((todo, index) => (
+        <li key={index}>
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={(e) => {
+              handleCompletedToDo(todo, index);
+            }}
+          />
+          {todo.todotext}
+          <button onClick={() => deleteToDo(index)}>Delete</button>
+        </li>
+      ))}
     </ul>
-  )
+  );
 }
 
-export default ToDoList
+export default ToDoList;
